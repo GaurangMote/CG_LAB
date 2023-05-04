069f7ca75a96a44630aade5e30b27eefc9b83e86
GaurangMote <129272470+GaurangMote@users.noreply.github.com>
2023-05-04 10:14:40 +0530

    Update ass5.cpp

--- ass5.cpp
+++ ass5.cpp
@@ -1,200 +1,155 @@
-#include<GL/glut.h>
- 
- 
-// TBRL
-int left = 1,right = 2, top = 8, bottom = 4;
-int xmin = 100, xmax = 400, ymin = 100, ymax = 400;
-int line_co_ordinate[4],i;
-float x1=200, y1=250, x2=450,y2=450;
-int c1,c2;
-int flag = 1,clip_flag=0;
- 
- 
-void myInit()
+#include <stdio.h>
+#include <GL/glut.h>
+#define outcode int
+double xmin = 50, ymin = 50, xmax = 100, ymax = 100; // Window boundaries
+double xvmin = 200, yvmin = 200, xvmax = 300, yvmax = 300; // Viewport boundaries
+//bit codes for the right, left, top, & bottom
+const int RIGHT = 8;
+const int LEFT = 2;
+const int TOP = 4;
+const int BOTTOM = 1;
+//used to compute bit codes of a point
+outcode ComputeOutCode(double x, double y);
+//Cohen-Sutherland clipping algorithm clips a line from
+//P0 = (x0, y0) to P1 = (x1, y1) against a rectangle with
+//diagonal from (xmin, ymin) to (xmax, ymax).
+void CohenSutherlandLineClipAndDraw(double x0, double y0, double x1, double y1)
 {
-    glClearColor(1.0, 1.0, 1.0, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.0, 0.0, 1.0);
-    gluOrtho2D(0, 500,0, 500);
-    glFlush();
+  //Outcodes for P0, P1, and whatever point lies outside the clip rectangle
+  outcode outcode0, outcode1, outcodeOut; bool accept = false, done = false;
+  //compute outcodes
+  outcode0 = ComputeOutCode(x0, y0);
+  outcode1 = ComputeOutCode(x1, y1);
+  do
+  {
+    if (!(outcode0 | outcode1)) //logical or is 0 Trivially accept & exit
+    {
+    accept = true; done = true;
+    }
+  else if (outcode0 & outcode1)
+  //logical and is not 0. Trivially reject and exit
+  done = true;
+  else
+  { //failed both tests, so calculate the line segment to clip
+  //from an outside point to an intersection with clip edge
+  double x, y;
+  //At least one endpoint is outside the clip rectangle; pick it.
+  outcodeOut = outcode0? outcode0: outcode1;
+  //Now find the intersection point;
+  //use formulas y = y0 + slope * (x - x0),
+  // x = x0 + (1/slope)* (y - y0)
+  if (outcodeOut & TOP)
+  //point is above the clip rectangle
+  {
+  x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0);
+  y = ymax;
+  }
+  else if (outcodeOut & BOTTOM)
+  //point is below the clip rectangle
+  {
+  x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0);
+  y = ymin;
+  }
+  else if (outcodeOut & RIGHT)
+  //point is to the right of clip rectangle
+  {
+  y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0);
+  x = xmax;
+  }
+  else
+  //point is to the left of clip rectangle
+  {
+  y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0);
+  x = xmin;
+  }
+  //Now we move outside point to intersection point to clip
+  //and get ready for next pass.
+  if (outcodeOut == outcode0)
+  {
+    x0 = x;
+    y0 = y;
+    outcode0 = ComputeOutCode(x0, y0);
+  }
+  else
+  {
+    x1 = x; y1 = y; outcode1 = ComputeOutCode(x1, y1);
+  }
+  }
+  }
+  while (!done);
+  if (accept)
+{ // Window to viewport mappings
+  double sx = (xvmax - xvmin) / (xmax - xmin); // Scale parameters
+  double sy = (yvmax - yvmin) / (ymax - ymin);
+  double vx0 = xvmin + (x0 - xmin)*sx;
+  double vy0 = yvmin + (y0 - ymin)*sy;
+  double vx1 = xvmin + (x1 - xmin)*sx;
+  double vy1 = yvmin + (y1 - ymin)*sy; //draw a red colored viewport
+  glColor3f(1.0, 0.0, 0.0);
+  glBegin(GL_LINE_LOOP);
+  glVertex2f(xvmin, yvmin);
+  glVertex2f(xvmax, yvmin);
+  glVertex2f(xvmax, yvmax);
+  glVertex2f(xvmin, yvmax);
+  glEnd();
+  glColor3f(0.0, 0.0, 1.0); // draw blue colored clipped line
+  glBegin(GL_LINES);
+  glVertex2d(vx0, vy0);
+  glVertex2d(vx1, vy1);
+  glEnd();
 }
-
- 
-int getCode(int x, int y)
+}
+//Compute the bit code for a point (x, y) using the clip rectangle
+//bounded diagonally by (xmin, ymin), and (xmax, ymax)
+outcode ComputeOutCode(double x, double y)
 {
-    int code = 0;
- 
-    if(x < xmin)
-    {
-        code = code | left;
-    }
-
-     
-    if(x > xmax)
-    {
-        code = code | right;
-    }
-     
-    if(y < ymin)
-    {
-        code = code | bottom;
-    }
-     
-    if(y > ymax)
-    {
-        code = code | top;
-    }
- 
-    return code;
+  outcode code = 0;
+  if (y > ymax) //above the clip window
+  code |= TOP;
+  else if (y < ymin) //below the clip window
+  code |= BOTTOM;
+  if (x > xmax) //to the right of clip window
+  code |= RIGHT;
+  else if (x < xmin) //to the left of clip window
+  code |= LEFT;
+  return code;
 }
- 
- 
-void clip()
-{
-    int c;
-    float x,y;
-    float m = (y2-y1)/(x2-x1);
-    if(c1)
-    {
-        c = c1;
-    }
-    else
-    {
-        c = c2;
-    }
- 
-    if( c & left)
-    {
-        x = xmin;
-        y = y1 + m*(xmin - x1) ;
-    }
- 
-    if( c & right)
-    {
-        x = xmax;
-        y = m*(xmax - x1) + y1; 
-    }
-     
-    if( c & top)
-    {
-        y = ymax;
-        x = ((ymax - y1)/m) + x1;
-    }
-     
-    if( c & bottom)
-    {
-        y = ymin;
-        x = ((ymin - y1)/m) + x1;
-    }    
- 
-    if(c == c1)
-    {
-        x1 = x;
-        y1 = y;
-    }
-    else
-    {
-        x2 = x;
-        y2 = y;
-    }
-     
-}
- 
 void display()
 {
- 
-    glClear(GL_COLOR_BUFFER_BIT); //clearing buffer
-    glColor3f(1.0, 0.0, 0.0);     //assigning color
-    glPointSize(4);
-    glBegin(GL_LINE_LOOP);
-        glVertex2f(xmin,ymin);
-        glVertex2f(xmax,ymin);
-        glVertex2f(xmax,ymax);
-        glVertex2f(xmin,ymax);
-    glEnd();    
-     
-    if(flag == 1)
-    {
-        glBegin(GL_LINES);
-        glVertex2f(x1,y1);
-        glVertex2f(x2,y2);
-        glEnd();
-    }   
-     
-    while(1 & clip_flag == 1) 
-   {
-        c1 = getCode(x1,y1);
-         
-        c2 = getCode(x2,y2);
- 
-        // Line both point inside
-        if((c1 | c2)==0)
-        break;
- 
-        // Line both point outside
-        else if((c1&c2) != 0)       
-        {
-            flag = 0;
-        break;
-        }
-        
-        else
-        {
-        clip();
-        }
-         
-         
-    }
-    glFlush();
-}  
- 
- 
-// Keyboard Func
-void Key(unsigned char ch,int x,int y)
-{
-        if(ch=='c')
-        {
-            clip_flag = 1;
-         
-             glutPostRedisplay();
-    }
+  double x0 = 60, y0 = 20, x1 = 80, y1 = 120;
+  glClear(GL_COLOR_BUFFER_BIT); //draw the line with red color
+  glColor3f(1.0,0.0,0.0); //bres(120,20,340,250);
+  glBegin(GL_LINES);
+  glVertex2d (x0, y0);
+  glVertex2d (x1, y1);
+  glEnd(); //draw a blue colored window
+  glColor3f(0.0, 0.0, 1.0);
+  glBegin(GL_LINE_LOOP);
+  glVertex2f(xmin, ymin);
+  glVertex2f(xmax, ymin);
+  glVertex2f(xmax, ymax);
+  glVertex2f(xmin, ymax);
+  glEnd();
+  CohenSutherlandLineClipAndDraw(x0, y0, x1, y1);
+  glFlush();
 }
- 
-// Mouse Func
-void mouse_input(int button,int state,int x,int y)
+void myinit()
 {
-    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && i < 4)
-    {
-        line_co_ordinate[i] = x;
-        i=i+1;
-        line_co_ordinate[i] = 500-y;
-        i++;
-    }
-    if(i==4)
-    {
-        i=0;
-        x1 = line_co_ordinate[0];
-        y1 = line_co_ordinate[1];
-        x2 = line_co_ordinate[2];
-        y2 = line_co_ordinate[3];   
-         
-        display();
-    }
+  glClearColor(1.0, 1.0, 1.0, 1.0);
+  glColor3f(1.0, 0.0, 0.0);
+  glPointSize(1.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, 499.0, 0.0, 499.0);
 }
- 
- 
-int main(int argc,char **argv)
+int main(int argc, char** argv)
 {
-    glutInit(&argc,argv);
-    glutInitWindowPosition(100,100);
-    glutInitWindowSize(500,500);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutCreateWindow("Cohen Suderland");
-    myInit();
-    glutDisplayFunc(display);
-    glutKeyboardFunc(Key);
-    glutMouseFunc(mouse_input);
-    glutMainLoop();
-    return 0;
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(0, 0);
+  glutCreateWindow("Cohen Suderland Line Clipping Algorithm");
+  glutDisplayFunc(display);
+  myinit();
+  glutMainLoop();
 }
-
