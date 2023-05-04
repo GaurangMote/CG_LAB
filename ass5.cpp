d129d59885249e0f3def84bbae37e3f6cd0f0a2a
GaurangMote <129272470+GaurangMote@users.noreply.github.com>
2023-05-04 09:53:41 +0530

    Update ass5.cpp

--- ass5.cpp
+++ ass5.cpp
@@ -1,143 +1,199 @@
 #include<GL/glut.h>
-#include<math.h>
-#include<stdio.h>
-#include<iostream>
-
-void display();
-using namespace std;
-float xmin=-100;
-float ymin=-100;
-float xmax=100;
-float ymax=100;
-float xd1,yd1,xd2,yd2;
-
-
-void init(void)
+ 
+ 
+// TBRL
+int left = 1,right = 2, top = 8, bottom = 4;
+int xmin = 100, xmax = 400, ymin = 100, ymax = 400;
+int line_co_ordinate[4],i;
+float x1=200, y1=250, x2=450,y2=450;
+int c1,c2;
+int flag = 1,clip_flag=0;
+ 
+ 
+void myInit()
 {
-
-    glClearColor(0.0,0,0,0);
-    glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(-300,300,-300,300);
-
+    glClearColor(1.0, 1.0, 1.0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0, 0.0, 1.0);
+    gluOrtho2D(0, 500,0, 500);
+    glFlush();
 }
 
-int code(float x,float y)
+ 
+int getCode(int x, int y)
 {
-    int c=0;
-    if(y>ymax)c=8;
-    if(y<ymin)c=4;
-    if(x>xmax)c=c|2;
-    if(x<xmin)c=c|1;
-    return c;
-}
-
-void cohen_Line(float x1,float y1,float x2,float y2)
-{
-    int c1=code(x1,y1);
-    int c2=code(x2,y2);
-    float m=(y2-y1)/(x2-x1);
-    while((c1|c2)>0)
+    int code = 0;
+ 
+    if(x < xmin)
     {
-        if((c1 & c2)>0)
-        {
-           exit(0);
-        }
-
-    float xi=x1;float yi=y1;
-    int c=c1;
-    if(c==0)
-    {
-         c=c2;
-         xi=x2;
-         yi=y2;
+        code = code | left;
     }
-    float x,y;
-    if((c & 8)>0)
+
+     
+    if(x > xmax)
     {
-       y=ymax;
-       x=xi+ 1.0/m*(ymax-yi);
+        code = code | right;
+    }
+     
+    if(y < ymin)
+    {
+        code = code | bottom;
+    }
+     
+    if(y > ymax)
+    {
+        code = code | top;
+    }
+ 
+    return code;
+}
+ 
+ 
+void clip()
+{
+    int c;
+    float x,y;
+    float m = (y2-y1)/(x2-x1);
+    if(c1)
+    {
+        c = c1;
     }
     else
-      if((c & 4)>0)
-      {
-          y=ymin;
-          x=xi+1.0/m*(ymin-yi);
-      }
-      else
-       if((c & 2)>0)
-       {
-           x=xmax;
-           y=yi+m*(xmax-xi);
-       }
-       else
-       if((c & 1)>0)
-       {
-           x=xmin;
-           y=yi+m*(xmin-xi);
-       }
-
-       if(c==c1)
-       {
-           xd1=x;
-           yd1=y;
-           c1=code(xd1,yd1);
-       }
-
-       if(c==c2)
-       {
-           xd2=x;
-           yd2=y;
-           c2=code(xd2,yd2);
-       }
-}
-
- display();
-
-}
-
-void mykey(unsigned char key,int x,int y)
-{
-    if(key=='c')
-    {  cout<<"Hello";
-        cohen_Line(xd1,yd1,xd2,yd2);
-        glFlush();
+    {
+        c = c2;
     }
+ 
+    if( c & left)
+    {
+        x = xmin;
+        y = y1 + m*(xmin - x1) ;
+    }
+ 
+    if( c & right)
+    {
+        x = xmax;
+        y = m*(xmax - x1) + y1; 
+    }
+     
+    if( c & top)
+    {
+        y = ymax;
+        x = ((ymax - y1)/m) + x1;
+    }
+     
+    if( c & bottom)
+    {
+        y = ymin;
+        x = ((ymin - y1)/m) + x1;
+    }    
+ 
+    if(c == c1)
+    {
+        x1 = x;
+        y1 = y;
+    }
+    else
+    {
+        x2 = x;
+        y2 = y;
+    }
+     
 }
+ 
 void display()
 {
-
-   glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.0,1.0,0.0);
-
-   glBegin(GL_LINE_LOOP);
-   glVertex2i(xmin,ymin);
-   glVertex2i(xmin,ymax);
-   glVertex2i(xmax,ymax);
-   glVertex2i(xmax,ymin);
-   glEnd();
-   glColor3f(1.0,0.0,0.0);
-   glBegin(GL_LINES);
-   glVertex2i(xd1,yd1);
-   glVertex2i(xd2,yd2);
-   glEnd();
-   glFlush();
-
-
-}
-
-
-int main(int argc,char** argv)
+ 
+    glClear(GL_COLOR_BUFFER_BIT); //clearing buffer
+    glColor3f(1.0, 0.0, 0.0);     //assigning color
+    glPointSize(4);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(xmin,ymin);
+        glVertex2f(xmax,ymin);
+        glVertex2f(xmax,ymax);
+        glVertex2f(xmin,ymax);
+    glEnd();    
+     
+    if(flag == 1)
+    {
+        glBegin(GL_LINES);
+        glVertex2f(x1,y1);
+        glVertex2f(x2,y2);
+        glEnd();
+    }   
+     
+    while(1 & clip_flag == 1) 
+   {
+        c1 = getCode(x1,y1);
+         
+        c2 = getCode(x2,y2);
+ 
+        // Line both point inside
+        if((c1 | c2)==0)
+        break;
+ 
+        // Line both point outside
+        else if((c1&c2) != 0)       
+        {
+            flag = 0;
+        break;
+        }
+        
+        else
+        {
+        clip();
+        }
+         
+         
+    }
+    glFlush();
+}  
+ 
+ 
+// Keyboard Func
+void Key(unsigned char ch,int x,int y)
 {
-    printf("Enter line co-ordinates:");
-    cin>>xd1>>yd1>>xd2>>yd2;
+        if(ch=='c')
+        {
+            clip_flag = 1;
+         
+             glutPostRedisplay();
+    }
+}
+ 
+// Mouse Func
+void mouse_input(int button,int state,int x,int y)
+{
+    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && i < 4)
+    {
+        line_co_ordinate[i] = x;
+        i=i+1;
+        line_co_ordinate[i] = 500-y;
+        i++;
+    }
+    if(i==4)
+    {
+        i=0;
+        x1 = line_co_ordinate[0];
+        y1 = line_co_ordinate[1];
+        x2 = line_co_ordinate[2];
+        y2 = line_co_ordinate[3];   
+         
+        display();
+    }
+}
+ 
+ 
+int main(int argc,char **argv)
+{
     glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(600,600);
-    glutInitWindowPosition(0,0);
-    glutCreateWindow("Clipping");
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(500,500);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutCreateWindow("Cohen Suderland");
+    myInit();
     glutDisplayFunc(display);
-    glutKeyboardFunc(mykey);
-    init();
+    glutKeyboardFunc(Key);
+    glutMouseFunc(mouse_input);
     glutMainLoop();
     return 0;
 }
